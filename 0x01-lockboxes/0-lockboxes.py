046f157ef84485fc8d02c40a06fd144fdd557a53
Bolakale <bolakalebb@gmail.com>
2023-07-06 19:41:54 +0000

    lock_boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+method that determines if all the boxes can be opened.
+"""
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+
+    keys = [0]  # Start with the keys in the first box
+
+    while keys:
+        current_box = keys.pop()
+
+        for key in boxes[current_box]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+
+    return all(unlocked)
