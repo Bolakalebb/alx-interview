c4c1f9ae58aaaaf9731b4af49387be4a2c3b2b40
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-07-13 20:32:39 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,8 +6,13 @@ needed to result in exactly n H characters in the file.
 
 
 def minOperations(n):
+    """
+    In a text file, there is a single character H.
+    Your text editor can execute only two operations in this file:
+    Copy All and Paste.
+    """
 
-if n <= 1:
+    if n <= 1:
         return 0
     num, div, numOfOperations = n, 2, 0
 
