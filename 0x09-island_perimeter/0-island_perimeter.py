e84d7fe06fbea5fd0b4bf34c7c9758b0823436e5
Bolakalebb <110998249+Bolakalebb@users.noreply.github.com>
2023-08-30 14:29:08 +0100

    island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,22 +7,18 @@ def island_perimeter(grid):
     """This function returns the perimeter of the island described in grid
     """
 
-    if not grid:
-        return 0  # empty grid has no perimeter
-    perimeter = 0  # perimeter of the island
-    for row in range(len(grid)):  # rows in grid
-        for col in range(len(grid[row])):  # columns in grid
-            """ if the cell is land (1) then check the perimeter of the cell
-                and add it to the perimeter of the island described in grid
-            """
-            if grid[row][col] == 1:
-                perimeter += 4
-                # check top cell if exists
-                if row > 0 and grid[row - 1][col] == 1:
-                    # subtract 2 from perimeter if top cell is land
-                    perimeter -= 2
-                    # check left cell if exists
-                if col > 0 and grid[row][col - 1] == 1:
-                    # subtract 2 from perimeter if left cell is land
-                    perimeter -= 2
-    return perimeter  # return perimeter of the island
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Start with 4 sides
+                
+                # Check left and top neighbors
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Subtract 2 for shared side
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Subtract 2 for shared side
+    
+    return perimeter
